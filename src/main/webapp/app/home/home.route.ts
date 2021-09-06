d7eb791f30882a926b2dd531a6a6a024f28c03e3
matebebn <72282954+matebebn@users.noreply.github.com>
2021-09-07 00:05:13 +0200

    Update home.route.ts

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,15 +1,13 @@
 import { Route } from '@angular/router';
 
 import { HomeComponent } from './home.component';
-import { UserManagementComponent } from './list/user-management.component';
 
-export const HOME_ROUTE: Route = [
-  {
+
+export const HOME_ROUTE: Route ={
+  
   path: '',
   component: HomeComponent,
   data: {
     pageTitle: 'home.title',
-  },
-  },
-  {
-];
+  }, 
+};
